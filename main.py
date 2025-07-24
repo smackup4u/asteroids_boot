407c63e57593d24fed3948ee026310d0134a6b2a
Andras Verbenyi <headhunter.germany@gmail.com>
2025-07-24 10:39:36 +0200

    Create asteroidfield, comments in main

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import pygame
 from constants import *
 import player
+import asteroid
+import asteroidfield
+
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    # Init pygame and creating containers
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     game_clock = pygame.time.Clock()
     dt = 0
+    #container definition for classes (Player, Asteroid, asteroidfield)
+    player.Player.containers = (updatable, drawable)
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_char = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = asteroidfield.AsteroidField()
+    # Gameloop (you can end the programm either by closing the game window or Ctrl+C in the commandline)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,12 +32,12 @@ def main():
         
         screen.fill(0)
         dt = game_clock.tick(60) / 1000
-        player_char.update(dt)
-        player_char.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        # player_char.draw(screen)
         pygame.display.flip()
         
-
-    
 
 
 
